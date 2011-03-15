f7a35b01874492065fee338391eb9aa5aa1f4d5c
Cyril Glapa <cyril.glapa@supinfo.com>
2011-03-15 21:33:46 +0100

    Ajout d'un fichier de configuration. Contient les identifiants par défaut de connexion à la base de donnée MySQL.

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Définition des constantes MySQL par défaut
+define('MYSQL_DEFAULT_HOST'     , 'localhost');
+define('MYSQL_DEFAULT_USER'     , 'root');
+define('MYSQL_DEFAULT_PASSWORD' , '');
+define('MYSQL_DEFAULT_DB'       , 'webdev');
+
+?>
