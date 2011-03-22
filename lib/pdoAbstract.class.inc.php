ece4d566c8e67053ace6f3bca895632fc9597cbc
Cyril Glapa <cyril.glapa@supinfo.com>
2011-03-22 21:27:21 +0100

    Nettoyage du fichier de la classe pdoAbstract

--- lib/pdoAbstract.class.inc.php
+++ lib/pdoAbstract.class.inc.php
@@ -1,13 +1,9 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of pdoAbstract
  *
  * @author thorr
+ * @version 0.1
  */
 class pdoAbstract {
     protected $_pdo;
