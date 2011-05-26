824066cface3bc1cd66036dcc4cd0c6e523ba2d8
Thörr <93255@supinfo.com>
2011-05-26 14:51:10 +0200

    Ajout fichier jquery + ajout champs dynamiques dans editFamily.php

--- editFamily.php
+++ editFamily.php
@@ -47,17 +47,32 @@ else {
         <head>
             <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
             <title></title>
+            <script type="text/javascript" src="jquery-1.6.1.min.js"></script>
+            <script type="text/javascript">
+                $(document).ready(function() {
+                    
+                })
+            </script>
         </head>
         <body>
             <form action="editFamily.php" method="post">
-                <label for="name">Name : </label><input type="text" id="name" name="name"/><br />
-                <label for="parentfamily">Parent Family : </label><select name="parentfamily" id="parentfamily">
+                <label for="name">Name : </label><input type="text" id="name" name="name" tabindex="1"/><br />
+                <label for="parentfamily">Parent Family : </label><select name="parentfamily" id="parentfamily" tabindex="2">
                     <option value="0">No parent family</option>
                     <?php foreach ($families as $value) { ?>
                     <option value="<?php echo $value->getId();?>"><?php echo $value->getName();?></option>
                     <?php 
                     }?>
                 </select> <br />
+                <table>
+                <?php for($i = 1; $i<=3; $i++) {
+                ?>
+                <tr>
+                    <td><label for="champ<?php echo $i;?>">Champ <?php echo $i; ?> : </label><input type="text" name="champ<?php echo $i; ?>" id="champ<?php echo $i; ?>" tabindex="<?php echo $i+2; ?>"/></td>
+                <td><label for="champ<?php echo $i+3; ?>">Champ <?php echo $i+3; ?> : </label><input type="text" name="champ<?php echo $i+3; ?>" id="champ<?php echo $i+3; ?>" tabindex="<?php echo $i+5; ?>"/></td>
+                </tr>
+                <?php } ?>
+                </table>
                 <input type="submit" value="Send !"/>
             </form>
         </body>
