36b479b8d100bd8eef0cc6d674cb3e6fe1e542ae
Thörr <93255@supinfo.com>
2011-05-30 18:50:33 +0200

    Amélioration du contructeur du model et adaptation du pdoManager

--- classes/Model/Dynamic/Value.php
+++ classes/Model/Dynamic/Value.php
@@ -1,45 +1,61 @@
 <?php
 
 class Model_Dynamic_Value {
-    private $id, $id_field, $id_article, $value;
+    private $_id, $_id_field, $_id_article, $_value;
+    private static $_manager;
     
-    function __construct($id, $id_field, $id_article, $value) {
-        $this->id = $id;
-        $this->id_field = $id_field;
-        $this->id_article = $id_article;
-        $this->value = $value;
+    function __construct($id = null) {
+        self::init();
+        if(is_null($id))
+        {
+            $this->_id = null;
+            $this->_id_field = null;
+            $this->_id_article = null;
+            $this->_value = null;
+        }
+        else {
+            $dynamicValue = self::$_manager->getDynamicValue($id);
+            $this->_id = $dynamicValue['id'];
+            $this->_id_field = $dynamicValue['id_field'];
+            $this->_id_article = $dynamicValue['id_article'];
+            $this->_value = $dynamicValue['value'];
+        }
+    }
+    
+    public static function init() {
+        if(!is_object(self::$_manager)) self::$_manager = new Pdo_Dynamic_Value();
     }
     
     public function getId() {
-        return $this->id;
+        return $this->_id;
     }
 
     public function setId($id) {
-        $this->id = $id;
+        $this->_id = $id;
     }
 
     public function getId_field() {
-        return $this->id_field;
+        return $this->_id_field;
     }
 
     public function setId_field($id_field) {
-        $this->id_field = $id_field;
+        $this->_id_field = $id_field;
     }
 
     public function getId_article() {
-        return $this->id_article;
+        return $this->_id_article;
     }
 
     public function setId_article($id_article) {
-        $this->id_article = $id_article;
+        $this->_id_article = $id_article;
     }
 
     public function getValue() {
-        return $this->value;
+        return $this->_value;
     }
 
     public function setValue($value) {
-        $this->value = $value;
+        $this->_value = $value;
     }
 
 }
