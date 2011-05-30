ab948f63a8bd75955631865a2282f6b9c1233810
Thörr <93255@supinfo.com>
2011-05-30 18:50:33 +0200

    Amélioration du contructeur du model et adaptation du pdoManager

--- classes/Pdo/Dynamic/Value.php
+++ classes/Pdo/Dynamic/Value.php
@@ -1,5 +1,10 @@
 <?php
 class Pdo_Dynamic_Value extends Pdo_Manager {
-    
+    public function getDynamicValue($id) {
+        $query = $this->pdo->prepare("SELECT * FROM dynamic_values WHERE id = :id");
+        $query->bindValue(":id", $id);
+        $query->execute();
+        return $query->fetch(PDO::FETCH_ASSOC);
+    }
 }
 ?>
