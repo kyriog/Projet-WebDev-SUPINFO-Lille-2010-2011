c27974e3cae3f469c029c26a9eb5a8f5b55fe8bb
Cyril Glapa <cyril.glapa@supinfo.com>
2011-06-06 16:43:40 +0200

    Correction de la méthode add_field de la classe Pdo_Dynamic_Field

--- classes/Pdo/Dynamic/Field.php
+++ classes/Pdo/Dynamic/Field.php
@@ -2,9 +2,10 @@
 class Pdo_Dynamic_Field extends Pdo_Manager {
     public function add_field($field) {
         $query = $this->pdo->prepare("INSERT INTO dynamic_fields(id_family, name) VALUES(:id_family, :name)");
-        $query->bindValue(":id_family", field.getId_family());
-        $query->bindValue(":name", field.getName());
-        $query->execute();   
+        $query->bindValue(":id_family", $field->getFamily());
+        $query->bindValue(":name", $field->getName());
+        $query->execute();
+        return $this->pdo->lastInsertId();
     }
     
     public function getDynamicField($id) {
