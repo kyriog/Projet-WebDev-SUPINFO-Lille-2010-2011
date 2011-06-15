954e8686dfd762d64bb03c167f67d56564854250
Thörr <93255@supinfo.com>
2011-06-15 11:32:14 +0200

    Ajout de la possiblité d'accèder au détail d'un article via son ID

--- viewArticle.php
+++ viewArticle.php
@@ -2,6 +2,8 @@
 session_start();
 require_once('config.php');
 require_once('autoload.php');
+
+//Si on essaye veut accèder aux articles d'une famille.
 if(isset($_GET['family'])) {
     ?>
 <!DOCTYPE html>
@@ -59,5 +61,40 @@ if(isset($_GET['family'])) {
     </body>
 </html>
 <?php 
+} 
 
-} ?>
+//Si on décidé d'accéder au détail d'un article, comme le demande l'énoncé ...
+elseif(isset($_GET['id'])) { 
+    $article = new Model_Article($_GET['id'])?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <style type="text/css">
+            table{
+                border-collapse: collapse;
+                border: 1px solid black;
+            }
+            th, td {
+                border: 1px solid black;
+                padding-left: 5px;
+                padding-right: 5px;
+            }
+        </style>
+        <table>
+            <caption>Article : <?php echo $article->getId(); ?></caption>
+            <tr>
+                <th>Numéro de référencement</th><th>Code bare</th><th>Quantité dispo</th><th>Description</th><th>Etat</th><th>Lieu de stockage</th>
+            <tr/>
+            <tr>
+                <td><?php echo $article->getId();?></td><td><?php echo $article->getBarcode()?></td><td><?php echo $article->getQuantity()?></td><td><?php echo $article->getDescription()?></td><td><?php echo $article->getState()?></td><td><?php echo $article->getPlace()?></td>
+            </tr>
+        </table>
+    </body>
+</html>    
+
+<?php }
+?>
