3c3a1c357b178d540779e57d3a26b5380ef14e97
Thörr <93255@supinfo.com>
2011-06-15 11:12:17 +0200

    Ajout méthode pour obtenir les articles d'une famille dans une array

--- classes/Pdo/Article.php
+++ classes/Pdo/Article.php
@@ -7,5 +7,17 @@ class Pdo_Article extends Pdo_Manager {
         $value = $query->fetch(PDO::FETCH_ASSOC);
         return $value;
     }
+    
+    public function getArticlesByFamily($id_family) {
+        $query = $this->pdo->prepare("SELECT id FROM articles WHERE family = :id_family");
+        $query->bindValue(":id_family", $id_family);
+        $query->execute();
+        $array = array();
+        while($value = $query->fetch(PDO::FETCH_ASSOC)) {
+            $article = new Model_Article($value['id']);
+            array_push($array, $article);
+        }
+        return $array;
+    }
 }
 ?>
