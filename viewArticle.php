72b3f392d73accd51eb1a9f6b559cf4c458bb7e4
Thörr <93255@supinfo.com>
2011-06-15 13:21:41 +0200

    Correction faute orthographe dans les commentaires

--- viewArticle.php
+++ viewArticle.php
@@ -3,7 +3,7 @@ session_start();
 require_once('config.php');
 require_once('autoload.php');
 
-//Si on essaye veut accèder aux articles d'une famille.
+//Si on veut accèder aux articles d'une famille.
 if(isset($_GET['family'])) {
     ?>
 <!DOCTYPE html>
@@ -11,6 +11,44 @@ if(isset($_GET['family'])) {
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
+        <script type="text/javascript">
+                //Fonction pour initialiser le xhr, suivant si l'utilisateur utilisateur utilise ou non
+                //un vrai navigateur (donc pas Internet Explorer ...)
+                function getXMLHttpRequest() {
+                        var xhr = null;
+
+                        if (window.XMLHttpRequest || window.ActiveXObject) {
+                                if (window.ActiveXObject) {
+                                        xhr = new ActiveXObject("Msxml2.XMLHTTP");
+                                } else {
+                                        xhr = new XMLHttpRequest();
+                                }
+                        } else {
+                                alert("Impossible to use AJAX on your web browser !");
+                        }
+
+                        return xhr;
+                }
+
+                function deleteArticle(idfamily, idarticle) {
+                    var xhr = getXMLHttpRequest();
+                    xhr.onreadystatechange = function () {
+                        if (xhr.readyState == XMLHttpRequest.DONE) {
+                            if(xhr.status == 200) {
+                                //L'Ã©lÃ©ment Ã  recharger, avec les donnÃ©es qu'il va recevoir.
+                                document.getElementById("articles").innerHTML = xhr.responseText;
+                            }
+                        }
+                    }
+                    //Les argument Ã  passer, en GET et POST.
+                    xhr.open("POST", "articlesTable.php");
+                    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                    xhr.send("family=" + idfamily + "&article=" + idarticle);
+
+                    //Permet d'Ã©viter le chargement du lien contenu dans le <a>.
+                    return false;
+                }
+            </script>
     </head>
     <body>
         <style type="text/css">
@@ -24,46 +62,17 @@ if(isset($_GET['family'])) {
                 padding-right: 5px;
             }
         </style>
-        <table>
-            <?php 
-            $family = new Model_Family($_GET['family']);
-            $dynamic_fields = $family->getDynamicFields();
-            ?>
-            <caption><a href="viewFamily.php">Family : <?php echo $family->getName()?></a></caption>
-            <tr>
-                <th>Numéro de référencement</th><th>Code bare</th><th>Quantité dispo</th><th>Description</th><th>Etat</th><th>Lieu de stockage</th>
-                <?php 
-                //On ajoute s'il y en les titre des champs dynamiques
-                foreach ($dynamic_fields as $dynamic_field) { ?>
-                <th><?php echo $dynamic_field->getName();?></th>
-                <?php }
-                ?>
-            </tr>
-            <?php
-                //On ajoute les articles dans le tableau
-                $articles = Model_Article::getArticlesOfFamily($family->getId());
-                foreach($articles as $article) {
-            ?>
-            <tr>
-                <td><?php echo $article->getId(); ?></td><td><?php echo $article->getBarcode(); ?></td><td><?php echo $article->getQuantity(); ?></td><td><?php echo $article->getDescription(); ?></td><td><?php echo $article->getState(); ?></td><td><?php echo $article->getPlace(); ?></td>
-                
-                <?php 
-                //On ajoute toujours s'il y en a les valeurs des champs dynamiques
-                foreach ($dynamic_fields as $dynamic_field) { ?>
-                <td><?php echo Model_Dynamic_Value::getValueByIds($dynamic_field->getId(), $article->getId());?></td>
-                <?php }
-                ?>
-            </tr>
-            <?php 
-                }
-                ?>
-        </table>
+        <div id="articles">
+        <?php
+        require_once "articlesTable.php";
+        ?>
+        </div>
     </body>
 </html>
 <?php 
 } 
 
-//Si on décidé d'accéder au détail d'un article, comme le demande l'énoncé ...
+//Si on a décidé d'accéder au détail d'un article, comme le demande l'énoncé ...
 elseif(isset($_GET['id'])) { 
     $article = new Model_Article($_GET['id'])?>
 <!DOCTYPE html>
