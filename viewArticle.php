1440e0fee8ad269e628c6b89f93a85c6efc60519
Thörr <93255@supinfo.com>
2011-06-15 11:15:33 +0200

    Ajout de commentaire

--- viewArticle.php
+++ viewArticle.php
@@ -31,19 +31,22 @@ if(isset($_GET['family'])) {
             <tr>
                 <th>Numéro de référencement</th><th>Code bare</th><th>Quantité dispo</th><th>Description</th><th>Etat</th><th>Lieu de stockage</th>
                 <?php 
+                //On ajoute s'il y en les titre des champs dynamiques
                 foreach ($dynamic_fields as $dynamic_field) { ?>
                 <th><?php echo $dynamic_field->getName();?></th>
                 <?php }
                 ?>
             </tr>
-            <?php 
+            <?php
+                //On ajoute les articles dans le tableau
                 $articles = Model_Article::getArticlesOfFamily($family->getId());
                 foreach($articles as $article) {
             ?>
             <tr>
                 <td><?php echo $article->getId(); ?></td><td><?php echo $article->getBarcode(); ?></td><td><?php echo $article->getQuantity(); ?></td><td><?php echo $article->getDescription(); ?></td><td><?php echo $article->getState(); ?></td><td><?php echo $article->getPlace(); ?></td>
-                <?php 
                 
+                <?php 
+                //On ajoute toujours s'il y en a les valeurs des champs dynamiques
                 foreach ($dynamic_fields as $dynamic_field) { ?>
                 <td><?php echo Model_Dynamic_Value::getValueByIds($dynamic_field->getId(), $article->getId());?></td>
                 <?php }
