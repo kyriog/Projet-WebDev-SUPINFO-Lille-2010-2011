b0310568a42a0270094308a16d6a00986947a0a9
Thörr <93255@supinfo.com>
2011-06-15 11:10:37 +0200

    Ajout de la méthode pour retouner la valeur d'un dynamic value

--- classes/Pdo/Dynamic/Value.php
+++ classes/Pdo/Dynamic/Value.php
@@ -6,5 +6,14 @@ class Pdo_Dynamic_Value extends Pdo_Manager {
         $query->execute();
         return $query->fetch(PDO::FETCH_ASSOC);
     }
+    
+    public function getValue($id_field, $id_article) {
+        $query = $this->pdo->prepare("SELECT value FROM dynamic_values WHERE id_field = :id_field AND id_article = :id_article");
+        $query->bindValue(":id_field", $id_field);
+        $query->bindValue(":id_article", $id_article);
+        $query->execute();
+        $value = $query->fetch(pdo::FETCH_ASSOC);
+        return $value['value'];
+    }
 }
 ?>
