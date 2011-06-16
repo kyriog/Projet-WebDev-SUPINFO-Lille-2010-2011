c1513a095cadaa3119aeb20af7d038332eee7d86
Thörr <93255@supinfo.com>
2011-06-16 11:05:02 +0200

    Correction d'un fail de git

--- viewArticle.php
+++ viewArticle.php
@@ -2,11 +2,8 @@
 session_start();
 require_once('config.php');
 require_once('autoload.php');
-<<<<<<< HEAD
 
 //Si on veut accèder aux articles d'une famille.
-=======
->>>>>>> parent of 954e868... Ajout de la possiblité d'accèder au détail d'un article via son ID
 if(isset($_GET['family'])) {
     ?>
 <!DOCTYPE html>
@@ -73,7 +70,6 @@ if(isset($_GET['family'])) {
     </body>
 </html>
 <?php 
-<<<<<<< HEAD
 } 
 
 //Si on a décidé d'accéder au détail d'un article, comme le demande l'énoncé ...
@@ -108,7 +104,5 @@ elseif(isset($_GET['id'])) {
         </table>
     </body>
 </html>    
-=======
->>>>>>> parent of 954e868... Ajout de la possiblité d'accèder au détail d'un article via son ID
-
+<?php
 } ?>
