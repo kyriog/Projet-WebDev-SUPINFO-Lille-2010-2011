4e0855b1ea03035f10d6aca6d6b8daecb2a9ad89
Thörr <93255@supinfo.com>
2011-06-16 17:31:24 +0200

    Ajout des méthodes addArticle() et editArticle()

--- classes/Pdo/Article.php
+++ classes/Pdo/Article.php
@@ -25,5 +25,29 @@ class Pdo_Article extends Pdo_Manager {
         $query->bindValue(":id", $id);
         return $query->execute();
     }
+    
+    public function add_article($article){
+        $query = $this->pdo->prepare('INSERT INTO articles(barcode, family, quantity, description, state, place) VALUES (:barcode, :family, :quantity, :description, :state, :place)');
+        $query->bindValue(':barcode', $article->getBarcode());
+        $query->bindValue(':family', $article->getFamily());
+        $query->bindValue(':quantity', $article->getQuantity());
+        $query->bindValue(':description', $article->getDescription());
+        $query->bindValue(':state', $article->getState());
+        $query->bindValue(':place', $article->getPlace());
+        $query->execute();
+        return $this->pdo->lastInsertId();
+    }
+
+    public function edit_article($article){
+        $query = $this->pdo->prepare('UPDATE articles SET barcode = :barcode, family = :family, quantity = :quantity, description = :description, state = :state, place = :place WHERE id = :id');
+        $query->bindValue(':id', $article->getId());
+        $query->bindValue(':barcode', $article->getBarcode());
+        $query->bindValue(':family', $article->getFamily());
+        $query->bindValue(':quantity', $article->getQuantity());
+        $query->bindValue(':description', $article->getDescription());
+        $query->bindValue(':state', $article->getState());
+        $query->bindValue(':place', $article->getPlace());        
+        $query->execute();
+    }
 }
 ?>
