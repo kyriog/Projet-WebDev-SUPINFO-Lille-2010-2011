c60c8facc351d840e285efd83a76438edf43c598
Cyril Glapa <cyril.glapa@supinfo.com>
2011-06-18 22:55:16 +0200

    Ajout de la gestion de la quantité au manager Pdo_Loan_Article

--- classes/Pdo/Loan/Article.php
+++ classes/Pdo/Loan/Article.php
@@ -2,9 +2,10 @@
 class Pdo_Loan_Article extends Pdo_Manager {
     
     public function add_loan_article($loan_article){
-        $query = $this->pdo->prepare('INSERT INTO loans_articles(loan, article, begindate, enddate) VALUES (:loan, :article, :begindate, :enddate)');
+        $query = $this->pdo->prepare('INSERT INTO loans_articles(loan, article, quantity, begindate, enddate) VALUES (:loan, :article, :quantity, :begindate, :enddate)');
         $query->bindValue(':customer', $loan_article->getLoan());
         $query->bindValue(':article', $loan_article->getArticle());
+        $query->bindValue(':quantity', $loan_article->getQuantity());
         $query->bindValue(':begindate', $loan_article->getBegindate());
         $query->bindValue(':enddate', $loan_article->getEnddate());
         $query->execute();
@@ -12,10 +13,11 @@ class Pdo_Loan_Article extends Pdo_Manager {
     }
 
     public function edit_loan_article($loan){
-        $query = $this->pdo->prepare('UPDATE loans_articles SET loan = :loan, article = :article, begindate = :begindate, enddate = :enddate WHERE id = :id');
+        $query = $this->pdo->prepare('UPDATE loans_articles SET loan = :loan, article = :article, quantity = :quantity, begindate = :begindate, enddate = :enddate WHERE id = :id');
         $query->bindValue(':id', $$loan->getId());
         $query->bindValue(':customer', $loan_article->getLoan());
         $query->bindValue(':article', $loan_article->getArticle());
+        $query->bindValue(':quantity', $loan_article->getQuantity());
         $query->bindValue(':begindate', $loan_article->getBegindate());
         $query->bindValue(':enddate', $loan_article->getEnddate());
         $query->execute();
