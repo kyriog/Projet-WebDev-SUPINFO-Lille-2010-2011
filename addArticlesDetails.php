1562caf61a83902a6cc3942494272ec586a0136b
Cyril Glapa <cyril.glapa@supinfo.com>
2011-06-18 15:13:16 +0200

    Amélioration de la lisibilité du fichier addArticlesDetailes.php

--- addArticlesDetails.php
+++ addArticlesDetails.php
@@ -2,20 +2,17 @@
 require_once('config.php');
 require_once('autoload.php');
 
-if(isset($_POST['family']))
-{
+if(isset($_POST['family'])):
 $family = new Model_Family($_POST['family']);
 $dynamic_fields = $family->getDynamicFields();
 $i = 0;
-echo "<table>";
-foreach ($dynamic_fields as $dynamic_field)
-{
 ?>
-<tr>
-    <td><label for="dynamicvalue[<?php echo $dynamic_field->getId(); ?>]"><?php echo $dynamic_field->getName();?> :</label></td>
-    <td><input id="dynamicvalue[<?php echo $dynamic_field->getId(); ?>"name="dynamicvalue[<?php echo $dynamic_field->getId(); ?>]" type="text"/></td>
-</tr>
-<?php } 
-echo "</table>";
-}
-?>
+<table>
+    <?php foreach ($dynamic_fields as $dynamic_field): ?>
+    <tr>
+        <td><label for="dynamicvalue[<?php echo $dynamic_field->getId(); ?>]"><?php echo $dynamic_field->getName();?> :</label></td>
+        <td><input id="dynamicvalue[<?php echo $dynamic_field->getId(); ?>"name="dynamicvalue[<?php echo $dynamic_field->getId(); ?>]" type="text"/></td>
+    </tr>
+    <?php endforeach ?>
+</table>
+<?php endif ?>
