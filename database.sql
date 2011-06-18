6dfad9d7e1150f9b81e11daf326f8f5ebca54469
Cyril Glapa <cyril.glapa@supinfo.com>
2011-06-18 14:46:10 +0200

    Ajout de l'auto-increment sur la table dynamic_values

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: localhost
--- Généré le : Mar 22 Mars 2011 à 15:11
+-- Généré le : Sam 18 Juin 2011 à 14:40
 -- Version du serveur: 5.5.8
 -- Version de PHP: 5.3.5
 
@@ -77,14 +77,14 @@ CREATE TABLE IF NOT EXISTS `dynamic_fields` (
 --
 
 CREATE TABLE IF NOT EXISTS `dynamic_values` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `id_field` int(10) unsigned NOT NULL,
   `id_article` int(10) unsigned NOT NULL,
   `value` varchar(30) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_field` (`id_field`,`id_article`),
   KEY `id_article` (`id_article`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
