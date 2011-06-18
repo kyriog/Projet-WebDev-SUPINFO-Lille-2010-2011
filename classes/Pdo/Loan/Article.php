183205a9942a676bc8c9b97e702f18b370b7a3d7
Cyril Glapa <cyril.glapa@supinfo.com>
2011-06-18 23:27:31 +0200

    Correction d'erreurs dans le manager Pdo_Loan_Article

--- classes/Pdo/Loan/Article.php
+++ classes/Pdo/Loan/Article.php
@@ -3,7 +3,7 @@ class Pdo_Loan_Article extends Pdo_Manager {
     
     public function add_loan_article($loan_article){
         $query = $this->pdo->prepare('INSERT INTO loans_articles(loan, article, quantity, begindate, enddate) VALUES (:loan, :article, :quantity, :begindate, :enddate)');
-        $query->bindValue(':customer', $loan_article->getLoan());
+        $query->bindValue(':loan', $loan_article->getLoan());
         $query->bindValue(':article', $loan_article->getArticle());
         $query->bindValue(':quantity', $loan_article->getQuantity());
         $query->bindValue(':begindate', $loan_article->getBegindate());
@@ -12,10 +12,10 @@ class Pdo_Loan_Article extends Pdo_Manager {
         return $this->pdo->lastInsertId();
     }
 
-    public function edit_loan_article($loan){
+    public function edit_loan_article($loan_article){
         $query = $this->pdo->prepare('UPDATE loans_articles SET loan = :loan, article = :article, quantity = :quantity, begindate = :begindate, enddate = :enddate WHERE id = :id');
-        $query->bindValue(':id', $$loan->getId());
-        $query->bindValue(':customer', $loan_article->getLoan());
+        $query->bindValue(':id', $loan_article->getId());
+        $query->bindValue(':loan', $loan_article->getLoan());
         $query->bindValue(':article', $loan_article->getArticle());
         $query->bindValue(':quantity', $loan_article->getQuantity());
         $query->bindValue(':begindate', $loan_article->getBegindate());
