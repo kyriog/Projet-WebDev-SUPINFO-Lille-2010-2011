21458ff86a2ca0a637a02648c8fe1a26bcbfa920
Thörr <93255@supinfo.com>
2011-06-19 20:59:24 +0200

    Ajout de la page de visualisation de tous les clients.

---
+++ viewCustomer.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+require_once('config.php');
+require_once('autoload.php');
+$customers = Model_Customer::getAllCustomers();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+        <script type="text/javascript" src="jquery-1.6.1.min.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function() {
+                $(".delete").click(function(){
+                    var id = $(this).attr("id").split('_')[1];
+                    if(confirm("Êtes-vous sûr de vouloir supprimer le client "+id+" ?")) {
+                        $.ajax({
+                            type: "POST",
+                            url: "articlesTable.php",
+                            data: "action=delete&article="+id,
+                            success: function() {
+                                $("#article_"+id).hide("200", function() {
+                                    $(this).remove();
+                                })
+                            }
+                        })
+                    }
+                })
+            })
+        </script>
+        <style type="text/css">
+            table {
+                border-collapse: collapse;
+            }
+            
+            td, th {
+                border: 1px black solid;
+                padding: 5px 5px 5px 5px;
+            }
+        </style>
+    </head>
+    <body>
+        <table>
+            <tr><th>Identifiant</th><th>Prénom</th><th>Nom</th><th>Téléphone</th><th>Structure</th><th>Fonction</th><th>Adresse</th></tr>
+            <?php foreach($customers as $customer) { 
+                $structure = new Model_Structure($customer->getStructure());?>
+            <tr>
+                <td><?php echo $customer->getId(); ?></td>
+                <td><?php echo $customer->getFname();?></td>
+                <td><?php echo $customer->getLname();?></td>
+                <td><?php echo $customer->getPhone();?></td>
+                <td><?php echo $structure->getName();?></td>
+                <td><?php echo $customer->getFunction();?></td>
+                <td><?php echo $customer->getAddress();?></td>
+            </tr>
+            <?php } ?>
+        </table>
+    </body>
+</html>
