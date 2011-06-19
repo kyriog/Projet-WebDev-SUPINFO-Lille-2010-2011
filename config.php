0d07e218662046ae8a3da9f14439ca6a092aa8b4
Thörr <93255@supinfo.com>
2011-06-19 23:12:40 +0200

    RAZ du fichier config.php

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 
 // Définition des constantes MySQL par défaut
-define('MYSQL_DEFAULT_HOST'     , 'localhost');
-define('MYSQL_DEFAULT_USER'     , 'root');
+define('MYSQL_DEFAULT_HOST'     , '');
+define('MYSQL_DEFAULT_USER'     , '');
 define('MYSQL_DEFAULT_PASSWORD' , '');
-define('MYSQL_DEFAULT_DB'       , 'webdev');
+define('MYSQL_DEFAULT_DB'       , '');
 
 // Définition du répertoire de l'installation
-define('BASE_PATH'              , '/opt/lampp/htdocs/Web_Dev');
+define('BASE_PATH'              , '');
 
 ?>
