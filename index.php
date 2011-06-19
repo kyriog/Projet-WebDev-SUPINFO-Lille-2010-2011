f208ff3e8981ba8071d7740d6ffc0eb2b421f95f
Thörr <93255@supinfo.com>
2011-06-19 23:25:28 +0200

    Ajout d'une page d'index

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
+<!DOCTYPE html>
 <html>
     <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
     </head>
     <body>
         <?php include_once 'search.php';?>
+        <?php include_once 'menu.php' ?>
     </body>
 </html>
